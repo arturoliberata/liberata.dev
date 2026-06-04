7a76223f764701cc5dc1489d28e82828b3793778
Arturo Seoane Liberata <arturo@liberata.dev>
2026-06-04 13:20:40 +0200

    replace next with react

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Bio from "./components/Bio";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Work from "./components/Work";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Work></Work>
       <Bio></Bio>
       <Footer></Footer>
+      <Analytics />
+      <SpeedInsights />
     </>
   );
 }
