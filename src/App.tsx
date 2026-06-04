f46ffcb28478f976eec08f7398266769ea48dbc0
Arturo Seoane Liberata <arturo@liberata.dev>
2026-06-04 13:12:07 +0200

    adding analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Bio from "./components/Bio";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Work from "./components/Work";
+import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Work></Work>
       <Bio></Bio>
       <Footer></Footer>
+      <Analytics />
     </>
   );
 }
